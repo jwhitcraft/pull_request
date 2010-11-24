d79c3a03988bebd970711ce18ca8f4b320b34cbb
Jon Whitcraft <jwhitcraft@mac.com>
2010-11-24 16:37:55 -0500

    Added Creative Commons License

--- pull_request.sh
+++ pull_request.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+# http://creativecommons.org/licenses/by-sa/3.0/
+
+
 REVISION=""
 
 WD=`pwd`
